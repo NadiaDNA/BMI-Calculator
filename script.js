b90033905944d10236d89a3c13cc48d4aa41b346
NadiaDNA <nadiadininurazizah@gmail.com>
2025-08-29 07:32:23 +0700

    copy rules

---
+++ script.js
@@ -0,0 +1,12 @@
+//KETENTUAN SOAL
+//Rumus BMI untuk satuan KG dan CM adalah Berat Badan/(Tinggi Badan/100)^2
+//Aplikasi harus menampilkan status BMI sesuai dengan standar berikut:
+//BMI Categories:
+//Underweight = <18.5
+//Normal weight = 18.5 – 24.9
+//Overweight = 25 – 29.9
+//Obesity = BMI of 30 or greater
+//Expected Result
+//Berat Badan 60 KG
+//Tinggi Badan 167 CM BMI = 60 / (167/100)^2 BMI = 60 / (2.8) BMI = 21.5 BMI Categories: Normal Weight
+
